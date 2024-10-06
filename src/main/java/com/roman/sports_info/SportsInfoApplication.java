12bf2d3f216cd307c027a28dc9f6a3e6f0f455c9
Md. Roman Islam Ali <romanislam897@gmail.com>
2024-10-06 21:04:27 +0600

    first comment

---
+++ src/main/java/com/roman/sports_info/SportsInfoApplication.java
@@ -0,0 +1,13 @@
+package com.roman.sports_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/sports_info/SportsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.sports_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
